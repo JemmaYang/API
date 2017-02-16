67deff17d1f3955de5bccbecd5f615716bad46ff
jemma_yang <jessicyoungvive@163.com>
2017-02-15 21:03:55 -0500

    test

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ http.listen(8080, function() {
 */
 
 
-http.listen(process.env.PORT, function() {
+http.listen(process.env.PORT ||3000, function() {
     console.log('app listening on port 3000.');
 });
 
